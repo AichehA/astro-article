f6cc18ba5bf2ffd8fa60ccb7e787e70f3d7aaab5
Aicheh Alexandre <alexandre.aicheh@gmail.com>
2024-07-23 23:37:36 +0200

    Remise en place du composant LanguageSwitch

--- src/components/ui/language-switch.tsx
+++ src/components/ui/language-switch.tsx
@@ -1,22 +1,14 @@
 import appConfig from "app.config";
-import { type CollectionEntry } from "astro:content";
+import { getCollection, type CollectionEntry } from "astro:content";
+import { useEffect, useState } from "react";
 
 const langs = appConfig.langs;
 
 interface LanguageSwitchProps {
   currentURL: string;
-  collectionArticles: CollectionEntry<"articles">[];
 }
 
-export function LanguageSwitch({
-  currentURL,
-  collectionArticles,
-}: LanguageSwitchProps) {
-  console.log("currentURL", currentURL);
-  console.log("collectionArticles", collectionArticles);
-
-  // const t: CollectionEntry<"articles"> = [];
-
+export function LanguageSwitch({ currentURL }: LanguageSwitchProps) {
   const potentialPath = appConfig.langs.map((lang) => {
     const slugArray = currentURL
       .replace(import.meta.env.BASE_URL, "")
@@ -31,50 +23,40 @@ export function LanguageSwitch({
     return slugArray.join("/");
   };
 
-  // const [allData, setAllData] = useState<CollectionEntry<"articles">[]>();
+  const [allData, setAllData] = useState<CollectionEntry<"articles">[]>([]);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const articles = await getCollection("articles");
+  useEffect(() => {
+    const fetchData = async () => {
+      const articles = await getCollection("articles");
 
-  //     setAllData(articles);
-  //   };
+      setAllData(articles);
+    };
 
-  //   fetchData();
-  // }, []);
+    fetchData();
+  }, []);
 
-  // console.log("window.location.pathname", window.location.pathname);
-  console.log("potentialPath", potentialPath);
-  // console.log("allData", allData);
+  /**
+   * Permet de filtrer si l'article est disponible dans plusieurs langues.
+   * Le résultat des boutons est retourné dans ordre du tableau de configuration "appConfig.langs" fr/using-mdx
+   */
+  const getLangs = allData.filter((doc) => {
+    return potentialPath.includes(getPathDoc(doc.slug, doc.data.folder));
+  });
 
   return (
     <>
       <ul>
-        {collectionArticles
-          .filter((doc) => {
-            console.log("doc slug :", doc.slug);
-            const path = getPathDoc(doc.slug, doc.data.folder);
-            console.log("path : ", path);
-            const result = potentialPath.includes(path);
-
-            console.log("result", result);
-
-            return result;
-          })
-          .map((doc) => (
-            <li>
-              <a
-                href={`${import.meta.env.BASE_URL}/${getPathDoc(
-                  doc.slug,
-                  doc.data.folder
-                )}`}
-              >
-                {langs.find((lang) => doc.slug.includes(lang))?.toUpperCase()}
-              </a>
-            </li>
-          ))}
-        {collectionArticles.map((doc) => (
-          <li>{doc.id}</li>
+        {getLangs.map((doc) => (
+          <li key={doc.id}>
+            <a
+              href={`${import.meta.env.BASE_URL}/${getPathDoc(
+                doc.slug,
+                doc.data.folder
+              )}`}
+            >
+              {langs.find((lang) => doc.slug.includes(lang))?.toUpperCase()}
+            </a>
+          </li>
         ))}
       </ul>
     </>
