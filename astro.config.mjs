daed0f65c8dd2d921c1cb467df53dd084606685e
Aicheh Alexandre <alexandre.aicheh@gmail.com>
2024-07-23 21:31:22 +0200

    Ajout de react et tailwind. Passage du composant language-switch en react.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://aicheha.github.io",
   base: "astro-article",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
---
+++ src/components/ui/language-switch.tsx
@@ -0,0 +1,62 @@
+import appConfig from "app.config";
+import { getCollection } from "astro:content";
+import { useEffect, useState } from "react";
+
+const langs = appConfig.langs;
+
+export function LanguageSwitch() {
+  const potentialPath = appConfig.langs.map((lang) => {
+    const slugArray = window.location.pathname
+      .replace(import.meta.env.BASE_URL, "")
+      .split("/")
+      .splice(2);
+    return slugArray.length ? lang + "/" + slugArray.join("/") : lang;
+  });
+
+  const getPathDoc = (slug: string, folder: string) => {
+    const slugArray = slug.split("/");
+    slugArray.splice(1, 0, folder);
+    return slugArray.join("/");
+  };
+
+  const [allData, setAllData] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const articles = await getCollection("articles");
+
+      setAllData(articles);
+    };
+
+    fetchData();
+  }, []);
+
+  // console.log("allData", allData);
+
+  /**
+   * Permet de filtrer si l'article est disponible dans plusieurs langues.
+   * Le résultat des boutons est retourné dans ordre du tableau de configuration "appConfig.langs" fr/using-mdx
+   */
+  const getLangs = allData.filter((doc) => {
+    return potentialPath.includes(getPathDoc(doc.slug, doc.data.folder));
+  });
+
+  return (
+    <>
+      <ul>
+        {getLangs.map((doc) => (
+          <li key={doc.id}>
+            <a
+              href={`${import.meta.env.BASE_URL}/${getPathDoc(
+                doc.slug,
+                doc.data.folder
+              )}`}
+            >
+              {langs.find((lang) => doc.slug.includes(lang))?.toUpperCase()}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
