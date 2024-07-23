a1edec86a8805341c62e20a345dd3c29e60f3308
Aicheh Alexandre <alexandre.aicheh@gmail.com>
2024-07-23 22:13:04 +0200

    DEBUG 5 : LanguageSwitch

--- src/components/ui/language-switch.tsx
+++ src/components/ui/language-switch.tsx
@@ -47,31 +47,27 @@ export function LanguageSwitch({
   console.log("potentialPath", potentialPath);
   // console.log("allData", allData);
 
-  /**
-   * Permet de filtrer si l'article est disponible dans plusieurs langues.
-   * Le résultat des boutons est retourné dans ordre du tableau de configuration "appConfig.langs" fr/using-mdx
-   */
-  const getLangs = collectionArticles.filter((doc) => {
-    return potentialPath.includes(getPathDoc(doc.slug, doc.data.folder));
-  });
-
-  console.log("getLangs", getLangs);
-
   return (
     <>
       <ul>
-        {getLangs.map((doc) => (
-          <li key={doc.id}>
-            <a
-              href={`${import.meta.env.BASE_URL}/${getPathDoc(
-                doc.slug,
-                doc.data.folder
-              )}`}
-            >
-              {langs.find((lang) => doc.slug.includes(lang))?.toUpperCase()}
-            </a>
-          </li>
-        ))}
+        {collectionArticles
+          .filter((doc) => {
+            return potentialPath.includes(
+              getPathDoc(doc.slug, doc.data.folder)
+            );
+          })
+          .map((doc) => (
+            <li key={doc.id}>
+              <a
+                href={`${import.meta.env.BASE_URL}/${getPathDoc(
+                  doc.slug,
+                  doc.data.folder
+                )}`}
+              >
+                {langs.find((lang) => doc.slug.includes(lang))?.toUpperCase()}
+              </a>
+            </li>
+          ))}
       </ul>
     </>
   );
