bf8861f7751cf7a4d0072820ab9caf3e726362bb
Aicheh Alexandre <alexandre.aicheh@gmail.com>
2024-07-28 17:34:10 +0200

    Simplification de la méthode d'affichage pour "LanguageSwitch"

--- src/components/ui/language-switch.tsx
+++ src/components/ui/language-switch.tsx
@@ -14,6 +14,12 @@ interface DocInfo {
   folder: string;
 }
 
+interface LanguageSwitchUi {
+  slug: string;
+  href: string;
+  langLabelle: string | undefined;
+}
+
 export function LanguageSwitch({ currentURL }: LanguageSwitchProps) {
   const potentialPath = appConfig.langs.map((lang) => {
     const slugArray = currentURL
@@ -57,14 +63,6 @@ export function LanguageSwitch({ currentURL }: LanguageSwitchProps) {
   }, []);
 
   /**
-   * Permet de filtrer si l'article est disponible dans plusieurs langues.
-   * Le résultat des boutons est retourné dans ordre du tableau de configuration "appConfig.langs" fr/using-mdx
-   */
-  const getLangs = allDoc.filter((doc) => {
-    return potentialPath.includes(getPathDoc(doc));
-  });
-
-  /**
    * Retourne l'url par rapport au document passer en paramètre
    *
    * @param doc Le document
@@ -74,17 +72,33 @@ export function LanguageSwitch({ currentURL }: LanguageSwitchProps) {
     return `${import.meta.env.BASE_URL}/${getPathDoc(doc)}`;
   };
 
+  /**
+   * Permet de filtrer si l'article est disponible dans plusieurs langues.
+   * Le résultat des boutons est retourné dans ordre du tableau de configuration "appConfig.langs" fr/using-mdx
+   */
+  const getLangs: LanguageSwitchUi[] = allDoc
+    .filter((doc) => potentialPath.includes(getPathDoc(doc)))
+    .map((doc: DocInfo) => {
+      return {
+        slug: doc.slug,
+        href: href(doc),
+        langLabelle: langs
+          .find((lang) => doc.slug.includes(lang))
+          ?.toUpperCase(),
+      };
+    });
+
   return (
     <ul>
-      {getLangs.map((doc: DocInfo) => (
+      {getLangs.map((doc: LanguageSwitchUi) => (
         <li key={doc.slug}>
           <a
-            href={href(doc)}
+            href={doc.href}
             className={cn(
-              `${currentURL == href(doc) ? "font-bold underline" : ""}`
+              `${currentURL == doc.href ? "font-bold underline" : ""}`
             )}
           >
-            {langs.find((lang) => doc.slug.includes(lang))?.toUpperCase()}
+            {doc.langLabelle}
           </a>
         </li>
       ))}
